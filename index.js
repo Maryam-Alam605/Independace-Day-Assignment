f37e52690fc3825973a1207839745af7c006505c
Maryam Alam <maryamalam605@gmail.com>
2025-08-16 22:53:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function readMoreOne() {
+  var paraOne = document.getElementById("para1");
+  paraOne.innerText =
+    "On 14th August 1947, Pakistan gained independence from British rule after a long struggle led by Quaid-e-Azam Muhammad Ali Jinnah and the Muslim League. It marked the birth of a separate homeland for Muslims of the subcontinent, where they could live with freedom, dignity, and their own identity";
+  paraOne.classList.add("color");
+  var img = document.getElementById("imgOne");
+  img.style.display = "none";
+}
+function paraTwo() {
+  var para2 = document.getElementById("para2");
+  para2.innerText =
+    "Independence gave us the right to live with dignity, practice our faith freely, and shape our own destiny. It reminds us to cherish and protect the hard-earned liberty of Pakistan. True freedom also empowers us to speak our minds, pursue education, and contribute to the growth and progress of our nation.  ";
+  para2.classList.add("color");
+  var img = document.getElementById("imgTwo");
+  img.style.display = "none";
+}
+
+function paraThree() {
+  var para3 = document.getElementById("para3");
+  para3.innerText =
+    "Muhammad Ali Jinnah united Muslims under one platform, led the All-India Muslim League, and strongly advocated the Two-Nation Theory. He negotiated with the British and countered Congress to secure a separate homeland. His leadership led to the creation of Pakistan.";
+  para3.classList.add("color");
+  var img = document.getElementById("imgThree");
+  img.style.display = "none";
+}
